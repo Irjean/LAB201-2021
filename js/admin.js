c25e676276e163c44cfb510e7f9fb42702de953e
Clément <c.spileers@outlook.fr>
2021-03-20 14:25:43 +0100

    bug fixes

--- js/admin.js
+++ js/admin.js
@@ -10,7 +10,7 @@ const articleImg = document.querySelector("#article-image");
 const articleTitle = document.querySelector("#article-title");
 const articleArticle = document.querySelector("#article-article");
 const addArticleBtn = document.querySelector("#add-article-btn");
-const articles = [];
+let articles = [];
 
 window.onload = () => {
     getArticleFromFirestore();
@@ -44,11 +44,25 @@ articleBackBtn.addEventListener("click", () => {
 addArticleBtn.addEventListener("click", addNewArticle);
 
 blogPage.addEventListener("click", (e)=>{
-    console.log(e)
+    let id = e.target.value;
+    if(e.target.id === "delete-button"){
+        if(confirm("Voulez-vous vraiment supprimer l'article ?")){
+          db.collection("articles").doc(id).delete()
+        .then(() => {
+            alert("Article supprimé !");
+        }).catch((error) => {
+            alert("L'article n'a pas pu être supprimé : ", error);
+        }); 
+        e.target.parentNode.parentNode.remove();
+        console.log(id, articles[id].ID)
+        articles = articles.filter(i => i.ID !== Number(id));
+    }
+        }
 })
 
 function getArticleFromFirestore(){
-    db.collection("articles").get().then((querySnapshot) => {
+    docRef = db.collection("articles").orderBy("ID", "asc");
+    docRef.get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             let data = doc.data();
             articles.push(data);
@@ -66,33 +80,33 @@ function showArticles(articles){
                             <td>${i.image}</td>
                             <td>${i.titre}</td>
                             <td>${i.article}</td>
-                            <td><button id="edit-button" class="button btn-outline-info p-1 mb-1">Edit</button><button id="delete-button" class="button btn-outline-danger p-1">Delete</button></td>
+                            <td><button value="${i.ID}" id="edit-button" class="button btn-outline-info p-1 m-1">Edit</button><button value="${i.ID}" id="delete-button" class="button btn-outline-danger p-1">Delete</button></td>
                         </tr>
         `;
     }).join("");
 };
 
-function updateArticleList(id, image, titre, article){
-    return `
-        <tr id="article">
-                            <td>${id}</td>
-                            <td>${image}</td>
-                            <td>${titre}</td>
-                            <td>${article}</td>
-                            <td><button class="button btn-outline-info p-1 mb-1">Edit</button><button class="button btn-outline-danger p-1">Delete</button></td>
-                        </tr>
-        `;
-}
-
 function addNewArticle(){
-    console.log(articles.length)
-    let id = articles.length.toString();
+    let id = 0;
+    for(let i = 0; i < articles.length; i++){
+        console.log(i, articles[i].ID,  "C LE DECOMPTE LA");
+        if(articles[i].ID !== i){
+            console.log(i, articles[i].ID,  "C PAS PAREIL");
+            id = i.toString();
+            break;
+        }
+        if((i+1) === articles.length){
+            console.log("C LA FIN")
+            id = (i+1).toString();
+        }
+    }
+    console.log(id)
     let image = "image";
     let title = articleTitle.value;
     let article = articleArticle.value;
 
-    db.collection("articles").doc(id).set({
-        ID: articles.length,
+    db.collection("articles").doc(id || "0").set({
+        ID: Number(id),
         image: image,
         titre: title,
         article: article
@@ -108,16 +122,35 @@ function addNewArticle(){
     articleArticle.value = "";
 
     articles[articles.length] = {
-        ID: article.length,
+        ID: Number(id),
         image: image,
         titre: title,
         article: article
     }
 
-    let html = updateArticleList(id, image, title, article);
+    articles.sort(sortNum);
+
+    let html = showArticles(articles);
+    articleList.innerHTML = "";
     articleList.innerHTML += html;
 
     blogPage.classList.add("blog-class");
     datesPage.classList.remove("dates-class");
     addArticlePage.classList.remove("add-article");
+}
+
+function deleteArticle(articleIDnum){
+
+    let articleID = articleIDnum.toString();
+
+    db.collection("cities").doc(articleID).delete()
+    .then(() => {
+        alert("Article supprimé !");
+    }).catch((error) => {
+        console.error("Erreur lors de la suppression: ", error);
+    });
+}
+
+function sortNum(a, b){
+    return a.ID - b.ID;
 }
